e6253e4205b269ae66d5fed00447347a00113471
ildar.a <ildar371@ya.ru>
2023-10-13 18:33:57 +0300

    correction in formula

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculate(int weight, double high) {
         double result1;
-        result1 = weight / (high * 2);
+        result1 = weight / (high * high);
         int result2 = (int) result1;
         return result2;
     }
